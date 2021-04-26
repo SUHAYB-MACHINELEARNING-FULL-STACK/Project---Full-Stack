d305195b8f20e82ef38bd533d53809f52309eeac
SUHAYB-MACHINELEARNING-FULL-STACK <83054516+SUHAYB-MACHINELEARNING-FULL-STACK@users.noreply.github.com>
2021-04-27 02:31:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-/*var a = {firstAnimal:{Animal:"Dog - male - 27 years old - Killi - Texas"},SecondAnimal:{Animal:"Cat - male - 20 years old - John - Kanada"}}
-a = ["Dog - male - 27 years old - Killi - Texas","Cat - male - 20 years old - John - Kanada","Dog"]
-var s=""
-$(".is-primary").click(function() {
-    for(let i=0; i<a.length; i++) {
-        for(let x=0; x<$("#text").val().length; x++) {
-            if(a[i].toLowerCase().includes($("#text").val().toLowerCase()[x]) && $("#text").val()!=" " && $("#text").val()!="" && !(s.includes(a[i]))) {
-                s+=`\n<li> ${a[i]/*a[i].split(" ")[0]*//*}</li>`
-            }
-        }    
-    }
-    if(s!="") {
-    $("#ol_search").css("text-align","left")
-    $("#ol_search").html(s)
-    } else {
-        $("#ol_search").css("text-align","center").html("<h1>There are no results</h1>")
-    }
-    s=""
-})*/
-
-/* a = 
-["Dog - 
-male - 27 years old - 
-Killi - Texas","Cat - male - 20 years old - John - Kanada","Dog"]
-*/
 var w=$(".is-searchResults").html()
 var s=w
 var b=
